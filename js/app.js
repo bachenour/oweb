838973ff82ae3e055f01393617bae10c1a5108e8
bachenour <bache.nour2@gmail.com>
2023-05-07 13:07:52 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    // Mise à jour de l'affichage du prix
+    $('.price-range').on('input', function () {
+        $('.price-value').text($(this).val());
+    });
+
+    // Mise à jour de l'affichage de l'expérience
+    $('.experience-input').on('input', function () {
+        $('.experience-value').text($(this).val() + ' ans');
+    });
+});
